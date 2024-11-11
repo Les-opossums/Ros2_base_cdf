8f2df6ab1cf9be0b6a98e4864c0af96639e47758
MartH76 <martin25.husser@gmail.com>
2024-11-11 14:39:29 +0100

    change name of package

--- src/ihm/setup.py
+++ src/ihm/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'IHM'
+package_name = 'ihm'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'interface = IHM.interface:main'
+            'interface = ihm.interface:main'
         ],
     },
 )
