b771c70754fbfd5a271aa2f4c798802d5f982b65
LBMWiksy <delgorguealexis@gmail.com>
2025-03-24 23:04:02 +0100

    Adding comm with real serial. To test

--- opossum_simu/scripts/mini_action_requester.py
+++ opossum_simu/scripts/mini_action_requester.py
@@ -6,15 +6,17 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
-from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.executors import ExternalShutdownException
 from ament_index_python.packages import get_package_share_directory
 import os
+import serial
 import yaml
-import time
+import functools
 
 # Msgs import
 from std_msgs.msg import String
+
 
 class Communication(Node):
     """Simulate the communication between the zynq and the raspi."""
@@ -24,97 +26,192 @@ class Communication(Node):
         self.declare_parameters(
             namespace="",
             parameters=[
+                ("simulation", rclpy.Parameter.Type.BOOL),
                 ("send_comm_topic", rclpy.Parameter.Type.STRING),
                 ("rcv_comm_topic", rclpy.Parameter.Type.STRING),
+                ("cards_name", rclpy.Parameter.Type.STRING_ARRAY),
+                ("command_topic", rclpy.Parameter.Type.STRING),
+                ("feedback_command_topic", rclpy.Parameter.Type.STRING),
+                ("frequency", rclpy.Parameter.Type.DOUBLE),
             ],
         )
         self._init_parameters()
-        self._init_subscribers()
         self._init_publishers()
+        self._init_subscribers()
         self.get_logger().info("Beacon detector node initialized.")
-        time.sleep(1)
-        self.send_cmd()
-        while True:
-            time.sleep(1)
-            self.sender()
 
     def _init_parameters(self: Node) -> None:
         """Initialise parameters of the node."""
-        msgs_path = os.path.join(
-            get_package_share_directory("cdf_msgs"), "resources"
-        )
-        comm_yaml_file = os.path.join(msgs_path, "com_msgs.yaml") 
+        msgs_path = os.path.join(get_package_share_directory("cdf_msgs"), "resources")
+        comm_yaml_file = os.path.join(msgs_path, "com_msgs.yaml")
         msgs_yaml_file = os.path.join(msgs_path, "format_msgs.yaml")
         with open(comm_yaml_file, "r") as file:
             self.comm_yaml = yaml.safe_load(file)
         with open(msgs_yaml_file, "r") as file:
             self.msgs_yaml = yaml.safe_load(file)
+        self.default_exec = MutuallyExclusiveCallbackGroup()
+        self.simulation = (
+            self.get_parameter("simulation").get_parameter_value().bool_value
+        )
+        self.get_logger().info(f"Simu: {self.simulation}")
+        if not self.simulation:
+            cards_name = (
+                self.get_parameter("cards_name")
+                .get_parameter_value()
+                .string_array_value
+            )
+            self.frequency = (
+                self.get_parameter("frequency").get_parameter_value().double_value
+            )
+            self.cards = {}
+            for name in cards_name:
+                self.get_logger().info(f"cards.{name}.port")
+                self.declare_parameter(
+                    f"cards.{name}.port", rclpy.Parameter.Type.STRING
+                )
+                port = (
+                    self.get_parameter(f"cards.{name}.port")
+                    .get_parameter_value()
+                    .string_value
+                )
+
+                self.declare_parameter(
+                    f"cards.{name}.baudrate", rclpy.Parameter.Type.INTEGER
+                )
+                baudrate = (
+                    self.get_parameter(f"cards.{name}.baudrate")
+                    .get_parameter_value()
+                    .integer_value
+                )
+                self.cards[name] = {"port": port, "baudrate": baudrate}
+                self.cards[name]["serial"] = self._init_card(name)
 
     def _init_subscribers(self: Node) -> None:
         """Initialise the subscribers of the node."""
-        self.rcv_comm_topic = (
-            self.get_parameter("rcv_comm_topic").get_parameter_value().string_value
+        command_topic = (
+            self.get_parameter("command_topic").get_parameter_value().string_value
         )
-        self.get_logger().info(f"IM INITITIITI ")
-        self.sub_comm_topic = self.create_subscription(
-            String,
-            self.rcv_comm_topic,
-            self.result_callback,
-            10,
-        )
+        if self.simulation:
+            self.rcv_comm_topic = (
+                self.get_parameter("rcv_comm_topic").get_parameter_value().string_value
+            )
+            self.sub_comm_topic = self.create_subscription(
+                String,
+                self.rcv_comm_topic,
+                self.result_card_simu,
+                10,
+            )
+            self.sub_command = self.create_subscription(
+                String, command_topic, self.send_card_simu, 10
+            )
+        else:
+            self.read_timer = self.create_timer(1 / self.freq, self.read_cards)
+            # Currently, only one card, so we have name = first card name
+            name = list(self.cards.keys())[0]
+            self.sub_command = self.create_subscription(
+                String, command_topic, functools.partial(self.send_card, name=name), 10
+            )
 
     def _init_publishers(self: Node) -> None:
         """Initialize publishers of the node."""
-        self.send_comm_topic = (
-            self.get_parameter("send_comm_topic").get_parameter_value().string_value
-        )
-        self.pub_comm_topic = self.create_publisher(String, self.send_comm_topic, 10)
+        if self.simulation:
+            self.send_comm_topic = (
+                self.get_parameter("send_comm_topic").get_parameter_value().string_value
+            )
+            self.pub_comm_topic = self.create_publisher(
+                String, self.send_comm_topic, 10
+            )
 
-    def result_callback(self, msg):
-        "Look at the result of the command send."
+        feedback_command_topic = (
+            self.get_parameter("feedback_command_topic")
+            .get_parameter_value()
+            .string_value
+        )
+        self.pub_feedback_command = self.create_publisher(
+            String, feedback_command_topic, 10
+        )
+
+    def _init_card(self, name):
+        while True:
+            self.get_logger().info("trying to connect to %s" % name)
+            try:
+                tested_serial = serial.Serial(
+                    "/dev/" + self.cards[name]["port"],
+                    self.cards[name]["baudrate"],
+                    timeout=0,
+                    write_timeout=0,
+                )
+                tested_serial.write("VERSION\n".encode("utf-8"))
+
+                rclpy.spin_once(self, timeout_sec=0.2, executor=self.default_exec)
+
+                all_data = tested_serial.read(tested_serial.in_waiting).decode("utf-8")
+                for line in all_data.split("\n"):
+                    splited = line.split(",")
+                    if splited[0] == "version" and splited[1] == name:
+                        return tested_serial
+                    else:
+                        raise ValueError("unknown card type")
+            except serial.SerialException as e:
+                self.get_logger().error(
+                    "Scanned serial port is already opened nor existing !"
+                )
+                print(e)
+            except ValueError:
+                self.get_logger().info("Unknown card found")
+                tested_serial.close()
+            self.get_logger().warn("Retrying to connect the '%s' card in 1s" % name)
+            rclpy.spin_once(self, timeout_sec=1, executor=self.default_exec)
+
+    def send_card(self, msg, name):
+        """Send the received message frome ActionSequencer to real card."""
+        self.cards[name]["serial"].write((msg.data + "\n").encode("utf-8"))
+
+    def read_cards(self):
+        """Read the messages that could have been sent by the Zynq."""
+        try:
+            for name in self.cards.keys():
+                serial_card = self.cards[name]["serial"]
+                char_available = serial_card.in_waiting
+                if char_available != 0:
+                    received_data = str(serial_card.read(char_available).decode("UTF8"))
+                    try:
+                        data_sequences = received_data.split("\n")
+                    except Exception:
+                        self.get_logger().info("Error While Decoding Data")
+                        data_sequences = []
+                    self.get_logger().info(f"data_sequences: {data_sequences}")
+                    for data in data_sequences:
+                        self.pub_feedback_command.publish(String(data=data))
+        except Exception as e:
+            self.get_logger().error("Unhandled Exception : %s" % e)
+
+    def result_card_simu(self, msg):
+        """Look at the result of the command send in simulation."""
         parser = msg.data.split()
         name = parser[0]
         args = parser[1:]
         if name not in self.comm_yaml:
-            self.get_logger().info(f"The name {name} does not exists in the yaml file com_msgs.")
+            self.get_logger().info(
+                f"The name {name} does not exists in the yaml file com_msgs."
+            )
         msg_type = self.comm_yaml[name]["receive"]
         if msg_type not in self.msgs_yaml:
-            self.get_logger().info(f"The message type {msg_type} does not exists in the yaml file format_msgs.")
-        result = {key: args[i] for i, key in enumerate(list(self.msgs_yaml[msg_type]["struct"].keys()))}
-        self.get_logger().info(f"Received:")
+            self.get_logger().info(
+                f"The message type {msg_type} does not exists in the yaml file format_msgs."
+            )
+        result = {
+            key: args[i]
+            for i, key in enumerate(list(self.msgs_yaml[msg_type]["struct"].keys()))
+        }
+        self.get_logger().info("Received:")
         for key in result.keys():
             self.get_logger().info(f"{key}: {result[key]}.")
 
-    def sender(self):
-        "Send to the card the command."
-        msg = String()
-        name = "GETODOM"
-        args = {
-            "enable": 1,
-            "freq": 40.0,
-            }
-        command = name
-        msg_type = self.comm_yaml[name]["send"]
-        for key in self.msgs_yaml[msg_type]["struct"].keys():
-            command += " " + str(args[key])
-        msg.data = command
+    def send_card_simu(self, msg):
+        """Send to the card the command in simulation."""
         self.pub_comm_topic.publish(msg)
 
-    def send_cmd(self):
-        "Send to the card the command."
-        msg = String()
-        name = "SETODOM"
-        args = {
-            "x": 1.,
-            "y": 1.2,
-            "t": 1.6,
-        }
-        command = name
-        msg_type = self.comm_yaml[name]["send"]
-        for key in self.msgs_yaml[msg_type]["struct"].keys():
-            command += " " + str(args[key])
-        msg.data = command
-        self.pub_comm_topic.publish(msg)
 
 def main(args=None):
     """Spin main loop."""
@@ -128,6 +225,7 @@ def main(args=None):
     finally:
         zynq_comm_simu_node.destroy_node()
         rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
--- opossum_simu/scripts/zynq_simu.py
+++ opossum_simu/scripts/zynq_simu.py
@@ -18,6 +18,7 @@ import time
 # Msgs import
 from std_msgs.msg import String
 
+
 class ZynqSimulation(Node):
     """Simulate the communication between the zynq and the raspi."""
 
@@ -31,8 +32,7 @@ class ZynqSimulation(Node):
                 ("real_position_topic", rclpy.Parameter.Type.STRING),
                 ("trigger_position_srv", rclpy.Parameter.Type.STRING),
                 ("compute_period", rclpy.Parameter.Type.DOUBLE),
-
-                # Others represents the robot conditions 
+                # Others represents the robot conditions
                 ("rcv_comm_topic", rclpy.Parameter.Type.STRING),
                 ("send_comm_topic", rclpy.Parameter.Type.STRING),
                 ("boundaries", rclpy.Parameter.Type.DOUBLE_ARRAY),
@@ -49,16 +49,24 @@ class ZynqSimulation(Node):
     def _init_parameters(self: Node) -> None:
         """Initialise parameters of the node."""
         is_random = self.get_parameter("random_moves").get_parameter_value().bool_value
-        angular_velocity = self.get_parameter("angular_velocity").get_parameter_value().double_value
-        linear_velocity = self.get_parameter("linear_velocity").get_parameter_value().double_value
-        boundaries = self.get_parameter("boundaries").get_parameter_value().double_array_value
-        self.compute_period = self.get_parameter("compute_period").get_parameter_value().double_value
-        self.robot_state = RobotState(is_random, linear_velocity, angular_velocity, boundaries)
-        msgs_path = os.path.join(
-            get_package_share_directory("cdf_msgs"), "resources"
+        angular_velocity = (
+            self.get_parameter("angular_velocity").get_parameter_value().double_value
         )
+        linear_velocity = (
+            self.get_parameter("linear_velocity").get_parameter_value().double_value
+        )
+        boundaries = (
+            self.get_parameter("boundaries").get_parameter_value().double_array_value
+        )
+        self.compute_period = (
+            self.get_parameter("compute_period").get_parameter_value().double_value
+        )
+        self.robot_state = RobotState(
+            is_random, linear_velocity, angular_velocity, boundaries
+        )
+        msgs_path = os.path.join(get_package_share_directory("cdf_msgs"), "resources")
         self.timers_clb = {}
-        comm_yaml_file = os.path.join(msgs_path, "com_msgs.yaml") 
+        comm_yaml_file = os.path.join(msgs_path, "com_msgs.yaml")
         msgs_yaml_file = os.path.join(msgs_path, "format_msgs.yaml")
         with open(comm_yaml_file, "r") as file:
             self.comm_yaml = yaml.safe_load(file)
@@ -77,7 +85,7 @@ class ZynqSimulation(Node):
             self.rcv_comm_topic,
             self.do_action_callback,
             10,
-            callback_group = callback_group,
+            callback_group=callback_group,
         )
 
     def _init_publishers(self: Node) -> None:
@@ -88,20 +96,26 @@ class ZynqSimulation(Node):
         self.pub_comm_topic = self.create_publisher(String, self.send_comm_topic, 10)
 
     def do_action_callback(self, msg):
-        "Do action needed corresponding to messages."
+        """Do action needed corresponding to messages."""
         self.get_logger().info(f"REQ: {msg.data}.")
         parser = msg.data.split()
         name = parser[0]
         args = parser[1:]
         if name not in self.comm_yaml:
-            self.get_logger().info(f"The name {name} does not exists in the yaml file com_msgs.")
+            self.get_logger().info(
+                f"The name {name} does not exists in the yaml file com_msgs."
+            )
         order = self.comm_yaml[name]
         msg_type = order["send"]
         if msg_type not in self.msgs_yaml:
-            self.get_logger().info(f"The message type {msg_type} does not exists in the yaml file format_msgs.")
-        args = {key: args[i] for i, key in enumerate(self.msgs_yaml[msg_type]["struct"].keys())}
+            self.get_logger().info(
+                f"The message type {msg_type} does not exists in the yaml file format_msgs."
+            )
+        args = {
+            key: args[i]
+            for i, key in enumerate(self.msgs_yaml[msg_type]["struct"].keys())
+        }
         result = self.process_action(name, args)
-
         if order["receive"] == "null":
             return
         command = name
@@ -110,32 +124,35 @@ class ZynqSimulation(Node):
             command += " " + str(result[key])
         msg.data = command
         self.pub_comm_topic.publish(msg)
-    
+
     def process_action(self, name, args):
+        """Execute the action requested."""
         if name == "GETODOM":
             if int(args["enable"]) == 1 and float(args["freq"]) != 0:
                 # self.timers["GETODOM"] = self.create_timer(args["freq"], self.get_odom())
                 pass
-            else: 
+            else:
                 pass
             result = {
-                "x": float(self.robot_state.position[0].item()), 
-                "y": float(self.robot_state.position[1].item()), 
-                "t": self.robot_state.angle, 
+                "x": float(self.robot_state.position[0].item()),
+                "y": float(self.robot_state.position[1].item()),
+                "t": self.robot_state.angle,
             }
-        elif name == "SETODOM":
-            self.get_logger().info(f"HERE")
+        elif name == "MOVE":
             obj = np.array([[float(args["x"])], [float(args["y"])], [float(args["t"])]])
             real_time = time.time()
             self.get_logger().info(f"{self.robot_state.angle}")
             self.get_logger().info(f"{float(obj[2].item())}")
-            while np.linalg.norm(self.robot_state.position[:2] - obj[:2]) > 0.005 or abs(self.robot_state.angle - float(obj[2].item())) > 0.01:
+            while (
+                np.linalg.norm(self.robot_state.position[:2] - obj[:2]) > 0.005
+                or abs(self.robot_state.angle - float(obj[2].item())) > 0.01
+            ):
                 self.robot_state.move(obj, real_time)
                 real_time = time.time()
                 time.sleep(self.compute_period)
             result = {"is_done": True}
-            self.get_logger().info(f"IM FINALLY END ")
         return result
+
 
 def main(args=None):
     """Spin main loop."""
@@ -149,6 +166,7 @@ def main(args=None):
     finally:
         zynq_comm_simu_node.destroy_node()
         rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
