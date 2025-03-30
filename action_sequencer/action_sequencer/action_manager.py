221d04ec443ee090c63c61b5e99a54c723630219
GregHsr <gregoire.husser17@gmail.com>
2025-03-30 22:16:56 +0200

    first action message move to

---
+++ action_sequencer/action_sequencer/action_manager.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Version:
+    def __init__(self, version):
+        self.version = version
+
+class Position:
+    def __init__(self, x, y, t):
+        self.x = x
+        self.y = y
+        self.t = t
+
+class Speed:
+    def __init__(self, vx, vy, vt):
+        self.vx = vx
+        self.vy = vy
+        self.vt = vt
+
+class PUMP_struct:
+    def __init__(self, pump_id, enable):
+        self.pump_id = pump_id
+        self.enable = enable
+
+class LED_struct:
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+class ODOM_struct:
+    def __init__(self, enable, freq):
+        self.enable = enable
+        self.freq = freq
+
+class SERVO_struct:
+    def __init__(self, servo_id, angle):
+        self.servo_id = servo_id
+        self.angle = angle
--- action_sequencer/scripts/action_sequencer_node.py
+++ action_sequencer/scripts/action_sequencer_node.py
@@ -1,1 +1,60 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from action_sequencer.action_manager import Version, Position, Speed
+from action_sequencer.action_manager import PUMP_struct, LED_struct
+from action_sequencer.action_manager import ODOM_struct, SERVO_struct
+
+
+class ActionManager(Node):
+    def __init__(self):
+        super().__init__("action_manager_node")
+        self._init_parameters()
+        self._init_publishers()
+        self.move_to(Position(1, 2, 3))
+
+    def _init_parameters(self) -> None:
+        self.declare_parameters(
+            namespace="",
+            parameters=[
+                ("pub_command", "command"),
+                ("feedback_topic", "feedback_command"),
+            ],
+        )
+
+    def _init_publishers(self):
+        self.pub_command = self.create_publisher(String,
+                                                 "command",
+                                                 10
+                                                 )
+        self.pub_feedback = self.create_subscription(String,
+                                                     "feedback_command",
+                                                     self.feedback_callback,
+                                                     10
+                                                     )
+
+    def feedback_callback(self, msg):
+        self.get_logger().info(f"Feedback received: {msg.data}")
+
+    def move_to(self, pos: Position):
+        self.get_logger().info(f"Moving to : {pos.x} {pos.y} {pos.t}")
+        self.pub_command.publish(String(
+            data=f"MOVE {pos.x} {pos.y} {pos.t}"
+        ))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    action_manager_node = ActionManager()
+    rclpy.spin(action_manager_node)
+    action_manager_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
