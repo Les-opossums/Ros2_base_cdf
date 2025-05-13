1d678cb91d53780f57c54ca6e93e97b9683a8bd8
GregHsr <gregoire.husser17@gmail.com>
2025-05-13 18:07:57 +0200

    add PUMP2
    Signed-off-by: GregHsr <gregoire.husser17@gmail.com>

--- opossum_action_sequencer/opossum_action_sequencer/match/script1.py
+++ opossum_action_sequencer/opossum_action_sequencer/match/script1.py
@@ -22,9 +22,11 @@ class Script(ActionManager):
             self.add_score(1)
             self.move_to(Position(0.6, 0.6, -1.2))
             self.pump(PUMP_struct(1, 1))
+            self.pump(PUMP_struct(2, 1))
             time.sleep(4)
             self.move_to(Position(0.6, 1.45, -1.2))
             self.pump(PUMP_struct(1, 0))
+            self.pump(PUMP_struct(2, 1))
             time.sleep(4)
 
 
