73662427a4bbcfb3f11ac3916b1c8ea5b1d2b1e5
GregHsr <gregoire.husser17@gmail.com>
2025-05-20 13:49:52 +0200

    adapt script for repo
    Signed-off-by: GregHsr <gregoire.husser17@gmail.com>

--- utils/script_gui.sh
+++ utils/script_gui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-cd "/home/greg/" || exit 1
+# cd "/home/greg/" || exit 1
 
 session="supervisor"
 
@@ -26,9 +26,9 @@ tmux split-window -h
 # tmux select-window -t $SESSION_NAME:0
 
 ## launch command in each panel
-tmux send-keys -t $session:0.0 'src_opossum && robot_domain && ros2 run plotjuggler plotjuggler' 'C-m'
-tmux send-keys -t $session:0.1 'src_opossum && robot_domain && rviz2' 'C-m'
-tmux send-keys -t $session:0.2 'src_opossum && robot_domain && ros2 launch opossum_dev_gui dev_gui.launch.py' 'C-m'
+tmux send-keys -t $session:0.0 'source ../../install/setup.bash && ros2 run plotjuggler plotjuggler' 'C-m'
+tmux send-keys -t $session:0.1 'source ../../install/setup.bash && rviz2' 'C-m'
+tmux send-keys -t $session:0.2 'source ../../install/setup.bash && ros2 launch opossum_dev_gui dev_gui.launch.py' 'C-m'
 tmux send-keys -t $session:0.3 ''
 #
 tmux attach-session -t $session
