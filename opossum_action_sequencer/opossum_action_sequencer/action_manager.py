f9df1a0d871a75df1d762585b3bd349982463d78
GregHsr <gregoire.husser17@gmail.com>
2025-05-20 11:14:02 +0200

    add stepper struct
    Signed-off-by: GregHsr <gregoire.husser17@gmail.com>

--- opossum_action_sequencer/opossum_action_sequencer/action_manager.py
+++ opossum_action_sequencer/opossum_action_sequencer/action_manager.py
@@ -6,11 +6,13 @@ class Version:
     def __init__(self, version):
         self.version = version
 
+
 class Position:
     def __init__(self, x, y, t):
         self.x = x
         self.y = y
         self.t = t
+
 
 class Speed:
     def __init__(self, vx, vy, vt):
@@ -18,10 +20,12 @@ class Speed:
         self.vy = vy
         self.vt = vt
 
+
 class PUMP_struct:
     def __init__(self, pump_id, enable):
         self.pump_id = pump_id
         self.enable = enable
+
 
 class LED_struct:
     def __init__(self, red, green, blue):
@@ -29,12 +33,19 @@ class LED_struct:
         self.green = green
         self.blue = blue
 
+
 class ODOM_struct:
     def __init__(self, enable, freq):
         self.enable = enable
         self.freq = freq
 
+
 class SERVO_struct:
     def __init__(self, servo_id, angle):
         self.servo_id = servo_id
         self.angle = angle
+
+
+class STEPPER_struct:
+    def __init__(self, mode):
+        self.mode = mode
