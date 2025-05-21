3c3c3851a43098d309f087997b1da4976c96f04b
GregHsr <gregoire.husser17@gmail.com>
2025-05-21 14:18:11 +0200

    desespoir
    Signed-off-by: GregHsr <gregoire.husser17@gmail.com>

--- opossum_action_sequencer/opossum_action_sequencer/match/script4.py
+++ opossum_action_sequencer/opossum_action_sequencer/match/script4.py
@@ -6,7 +6,7 @@ from opossum_action_sequencer.scripts.action_sequencer_node import ActionManager
 from opossum_action_sequencer.action_manager import Version, Position, Speed
 from opossum_action_sequencer.action_manager import PUMP_struct, LED_struct
 from opossum_action_sequencer.action_manager import ODOM_struct, SERVO_struct
-from opossum_action_sequencer.action_manager import STEPPER_struct
+# from opossum_action_sequencer.action_manager import STEPPER_struct
 import time
 import threading
 
@@ -27,26 +27,27 @@ class Script():
             time.sleep(2)
             node.send_raw('SERVO 1 20')
             node.pump(PUMP_struct(1, 1))
+            node.pump(PUMP_struct(1, 1))
             node.send_raw('PUMP 1 1')
             node.send_raw('PUMP 2 1')
             node.pump(PUMP_struct(2, 1))
             node.pump(PUMP_struct(3, 1))
             node.move_to(Position(1.1, 0.9, -1.12), seuil=0.05)
             node.wait_for_motion()
-            node.stepper(STEPPER_struct(2))
+            # node.stepper(STEPPER_struct(2))
             node.pump(PUMP_struct(3, 1))
             node.send_raw('SERVO 1 180')
             time.sleep(2)
-            node.stepper(STEPPER_struct(1))
+            # node.stepper(STEPPER_struct(1))
 
             node.move_to(Position(0.35, 1.7, -0.95))
             node.wait_for_motion()
-            node.stepper(STEPPER_struct(2))
+            # node.stepper(STEPPER_struct(2))
             time.sleep(2)
             node.pump(PUMP_struct(1, 0))
             node.pump(PUMP_struct(2, 0))
             node.pump(PUMP_struct(3, 0))
-            node.stepper(STEPPER_struct(1))
+            # node.stepper(STEPPER_struct(1))
             node.add_score(10)
 
             node.send_raw('VMAX 0.7')
@@ -55,7 +56,7 @@ class Script():
             node.move_to(Position(1.05, 0.37, -2.6))
             node.wait_for_motion()
 
-            break
+            # break
 
     def stop(self):
         self._stop_event.set()
