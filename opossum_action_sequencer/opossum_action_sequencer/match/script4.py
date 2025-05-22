c7d0acacdde3e5cf61ec139e512705f56b6a7435
GregHsr <gregoire.husser17@gmail.com>
2025-05-22 13:32:20 +0200

    sequence depose complete
    Signed-off-by: GregHsr <gregoire.husser17@gmail.com>

--- opossum_action_sequencer/opossum_action_sequencer/match/script4.py
+++ opossum_action_sequencer/opossum_action_sequencer/match/script4.py
@@ -42,25 +42,32 @@ class Script():
             # Ramassage planche
             node.pump(PUMP_struct(2, 1))
             node.stepper(STEPPER_struct(2))
-            time.sleep(2)
 
-            # Eteint pompe bas et monte plance et conserve cote
+            # Eteint pompe bas et recule
             node.pump(PUMP_struct(1, 0))
+            node.move_to(Position(0.35, 1.55, -0.95), seuil=0.05)
+            node.wait_for_motion()
 
+            # Monte les objets
             node.stepper(STEPPER_struct(1))
+            time.sleep(1)
             node.send_raw('SERVO 1 20')
             node.send_raw('SERVO 2 180')
             time.sleep(1)
+
+            # Avance et pose les objets
+            node.move_to(Position(0.35, 1.7, -0.95), seuil=0.05)
+            node.wait_for_motion()
             node.pump(PUMP_struct(3, 0))
             node.pump(PUMP_struct(4, 0))
-            node.stepper(STEPPER_struct(1))
             time.sleep(2)
-            node.send_raw('SERVO 1 180')
-            node.send_raw('SERVO 2 20')
             node.add_score(10)
 
             node.send_raw('VMAX 0.7')
+            time.sleep(1)
             node.move_to(Position(0.35, 1.55, -0.95))
+            node.send_raw('SERVO 1 180')
+            node.send_raw('SERVO 2 20')
             node.wait_for_motion()
             node.move_to(Position(1.05, 0.37, -2.6))
             node.wait_for_motion()
