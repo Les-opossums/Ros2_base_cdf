1748672c7ed50ba89992b79024272653ec1d1e1b
GregHsr <gregoire.husser17@gmail.com>
2025-05-22 17:33:40 +0200

    wait for server to be ready
    Signed-off-by: GregHsr <gregoire.husser17@gmail.com>

--- utils/script_service.sh
+++ utils/script_service.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+while ! xset q &>/dev/null; do
+    sleep 1
+    echo "Waiting for X server to start..."
+done
+
 source install/setup.bash
 ros2 launch opossum_bringup debug.launch.py
