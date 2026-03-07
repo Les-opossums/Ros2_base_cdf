063d0f8630f0216847edaab823ac99d7adf572f2
GregHsr <gregoire.husser17@gmail.com>
2026-03-07 12:00:37 +0100

    script homolog

--- opossum_action_sequencer/opossum_action_sequencer/match/script_homologation.py
+++ opossum_action_sequencer/opossum_action_sequencer/match/script_homologation.py
@@ -3,12 +3,7 @@
 
 
 from opossum_action_sequencer.scripts.action_sequencer_node import ActionManager
-from opossum_action_sequencer.action_manager import Version, Position, Speed
-from opossum_action_sequencer.action_manager import PUMP_struct, LED_struct
-from opossum_action_sequencer.action_manager import ODOM_struct, SERVO_struct
-from opossum_action_sequencer.action_manager import STEPPER_struct
-from opossum_action_sequencer.action_manager import VALVE_struct
-import time
+from opossum_action_sequencer.action_manager import Position, VACCUMGRIPPER_struct
 from rclpy.logging import get_logger
 
 
@@ -22,35 +17,30 @@ class Script:
 
     def run(self, node):
         node.write_log("Script PD is running...")
-        node.end_zone = Position(1.5, 1.0, 0.0)
+        node.end_zone = Position(1., 1.0, 0.0)
 
-        for k in range(20):
-            # node.send_raw("VMAX 1.8")
-            # node.move_to(Position(2.4, 1.65, 0))
-            # node.wait_for_motion()
-            # node.sleep(0.5)
+        node.send_raw("VMAX 0.5")
+        node.move_to(Position(1., 1., 3.14))
+        node.wait_for_motion()
+        node.sleep(0.5)
 
-            node.send_raw("VMAX 1.8")
-            node.move_to(Position(2.4, 0.35, 0))
-            node.wait_for_motion()
-            node.sleep(0.5)
+        node.move_to(Position(0.9, 1., 3.14))
+        node.wait_for_motion()
+        node.sleep(0.5)
 
-            node.send_raw("VMAX 1.8")
-            node.move_to(Position(0.6, 0.35, 0))
-            node.wait_for_motion()
-            node.sleep(0.5)
+        node.vaccumgripper(VACCUMGRIPPER_struct(0, 1, 2))
+        node.vaccumgripper(VACCUMGRIPPER_struct(1, 1, 2))
+        node.sleep(2)
 
-            # node.send_raw("VMAX 1.8")
-            # node.move_to(Position(0.6, 1.65, 0))
-            # node.wait_for_motion()
-            # node.sleep(0.5)
+        node.move_to(Position(0.5, 0.5, 0.))
+        node.wait_for_motion()
+        node.sleep(0.5)
 
-            # node.send_raw("VMAX 1.8")
-            # node.move_to(Position(0.6, 0.35, 0))
-            # node.wait_for_motion()
-            # node.sleep(0.5)
+        node.vaccumgripper(VACCUMGRIPPER_struct(0, 2, 2))
+        node.vaccumgripper(VACCUMGRIPPER_struct(1, 3, 2))
+        node.sleep(2)
 
-            # node.send_raw("VMAX 1.8")
-            # node.move_to(Position(2.4, 0.35, 0))
-            # node.wait_for_motion()
-            # node.sleep(0.5)
+
+
+
+
