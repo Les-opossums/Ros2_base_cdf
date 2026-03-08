f9c08bae37f8edb2ee9dbb42a10662a14b35d9d0
GregHsr <gregoire.husser17@gmail.com>
2026-03-08 21:33:46 +0100

    to adapt with correct service

---
+++ opossum_ihm/opossum_ihm/interface_bringup.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+import subprocess
+
+class LaunchSelectorApp:
+    def __init__(self, name="Launcher"):
+        self.name = name
+        self.root = tk.Tk()
+        self.root.title(f"[{self.name}] Choix du plateau")
+
+        # Gestion de l'écran (Plein écran si 480x800)
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        if screen_width == 480 and screen_height == 800:
+            self.root.attributes("-fullscreen", True)
+        else:
+            self.root.geometry("480x800")
+            
+        self.label = tk.Label(self.root, text="Choisissez la configuration à lancer :", font=("Arial", 14))
+        self.label.pack(pady=30)
+
+        # Bouton Petit Plateau
+        self.button_petit = tk.Button(
+            self.root,
+            text="Petit Plateau",
+            bg="lightblue",
+            font=("Arial", 16, "bold"),
+            command=self.launch_petit_plateau,
+            width=30,
+            height=10,
+        )
+        self.button_petit.pack(pady=20)
+
+        # Bouton Grand Plateau
+        self.button_grand = tk.Button(
+            self.root,
+            text="Grand Plateau",
+            bg="lightgreen",
+            font=("Arial", 16, "bold"),
+            command=self.launch_grand_plateau,
+            width=30,
+            height=10,
+        )
+        self.button_grand.pack(pady=20)
+
+        self.root.mainloop()
+
+    def launch_petit_plateau(self):
+        print("Lancement de la configuration : Petit Plateau")
+        commande = ["ros2", "launch", "opossum_simu", "bringup_small_area.launch.py"]
+        subprocess.Popen(commande)
+        self.root.destroy()
+
+    def launch_grand_plateau(self):
+        commande = ["ros2", "launch", "opossum_simu", "bringup_simu.launch.py"]     
+        subprocess.Popen(commande)
+        self.root.destroy()
+
+if __name__ == "__main__":
+    app = LaunchSelectorApp("Robot Config")
